efab23f8ced95e202a7d5576573130a06c85d6dc
Ramon Durães <ramonduraes@outlook.com>
2025-06-29 07:32:25 -0300

    Update Server / Client

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol; 
+
+// Initialize MCP client
+var mcpClient = await McpClientFactory.CreateAsync(
+    new SseClientTransport(
+        new SseClientTransportOptions
+        {
+            Name = "MCP",
+            Endpoint = new Uri("http://localhost:5000/mcp")
+        }
+    )
+);
+
+// Call the "ping" tool
+var pingResponse = await mcpClient.CallToolAsync(
+    "ping",
+    new Dictionary<string, object?> { ["name"] = "Ramon" }
+);
+
+// Extract result
+var responseText = pingResponse.Content
+    .OfType<TextContentBlock>()
+    .Select(tb => tb.Text)
+    .FirstOrDefault() 
+    ?? "no response";
+
+// print result
+Console.WriteLine(responseText);
